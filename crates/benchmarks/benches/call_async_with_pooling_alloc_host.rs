0574f74f021f0bc6dac80d722e44c1772f07827e
sammyne <xiangmin-li@qq.com>
2026-02-06 06:20:53 +0000

    refactor(benchmarks/call_async_with_pooling_alloc_host): remove tokio executor

--- crates/benchmarks/benches/call_async_with_pooling_alloc_host.rs
+++ crates/benchmarks/benches/call_async_with_pooling_alloc_host.rs
@@ -1,6 +1,7 @@
 use std::path::PathBuf;
 
 use anyhow::{Context, Result};
+use criterion::async_executor::FuturesExecutor;
 use criterion::{Criterion, criterion_group, criterion_main};
 use engine::v21::component::{Component as ComponentV21, Linker as LinkerV21, Val as ValV21};
 use engine::v21::{self, Config as ConfigV21, Engine as EngineV21, Store as StoreV21};
@@ -47,7 +48,7 @@ fn setup_engine_v41_with_host(path: &PathBuf) -> Result<(EngineV41, ComponentV41
 }
 
 /// Benchmark call performance for v21 engine with host interface
-fn benchmark_call_with_host_v21(c: &mut Criterion, wasm_file: &str, func_name: &str, params: &[ValV21]) {
+fn benchmark_v21(c: &mut Criterion, wasm_file: &str, func_name: &str, params: &[ValV21]) {
     let wasm_path = get_golden_wasm_path(wasm_file);
     let (engine, component) = setup_engine_v21_with_host(&wasm_path).expect("Setup v21 failed");
     let mut linker = LinkerV21::new(&engine);
@@ -72,19 +73,13 @@ fn benchmark_call_with_host_v21(c: &mut Criterion, wasm_file: &str, func_name: &
         .expect(&format!("get short func_name from {func_name}"))
         .1;
 
-    let rt = tokio::runtime::Builder::new_multi_thread()
-        .worker_threads(8)
-        .enable_all()
-        .build()
-        .expect("build tokio");
-
     let group_name = format!(
         "call_async_with_pooling_alloc_host_{}_{}_v21",
         wasm_file.replace(".wasm", ""),
         func_name_short
     );
     c.bench_function(&group_name, move |b| {
-        b.to_async(&rt).iter(|| async move {
+        b.to_async(FuturesExecutor).iter(|| async {
             let (mut store, instance) = setup().await;
 
             let func = v21::find_func(&instance, &mut store, func_name).expect("find func");
@@ -129,14 +124,8 @@ fn benchmark_call_with_host_v41(c: &mut Criterion, wasm_file: &str, func_name: &
         func_name_short
     );
 
-    let rt = tokio::runtime::Builder::new_multi_thread()
-        .worker_threads(8)
-        .enable_all()
-        .build()
-        .expect("build tokio");
-
     c.bench_function(&group_name, move |b| {
-        b.to_async(&rt).iter(|| async move {
+        b.to_async(FuturesExecutor).iter(|| async {
             let (mut store, instance) = setup().await;
 
             let func = v41::find_func(&instance, &mut store, func_name).expect("find func");
@@ -156,7 +145,7 @@ fn benchmark_call_host_caller_echo_v21(c: &mut Criterion) {
 
     let params = [times];
 
-    benchmark_call_with_host_v21(c, "host-caller.wasm", "sammyne:host-caller/api@1.0.0#echo", &params);
+    benchmark_v21(c, "host-caller.wasm", "sammyne:host-caller/api@1.0.0#echo", &params);
 }
 
 /// Benchmark host-caller.wasm echo function with v41
